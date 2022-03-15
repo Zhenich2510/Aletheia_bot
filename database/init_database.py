8bebec529a9e7951497cc8c465102de8f715efec
yevheniy <yevhen.01@gmail.com>
2022-03-15 19:13:51 +0200

    Fixed a bug in command "insert into...".

--- database/init_database.py
+++ database/init_database.py
@@ -1,17 +1,17 @@
 import sqlite3
 
-from conf import link_to_bd
+from conf import link_to_database, database_name
 
 
 def start_db():
     global connect_db, cursor_db
-    connect_db = sqlite3.connect(link_to_bd)
-    cursor_db = sqlite3.cursor()
+    connect_db = sqlite3.connect(link_to_database)
+    cursor_db = connect_db.cursor()
 
     if connect_db:
         print("Database is work.")
 
-    connect_db.execute("""CREATE TABLE IF NOT EXIST db(
+    connect_db.execute(f"""CREATE TABLE IF NOT EXISTS {database_name}(
     id INTEGER PRIMARY KEY, 
     user_id INTEGER NOT NULL,
     user_nickname CHAR(20) NOT NULL,
@@ -24,5 +24,9 @@ def start_db():
 
 
 def add_info_to_db(user_id: int, user_nickname: str, user_text: str, predict: bool):
-    cursor_db.execute("INSERT INTO db VALUES (?, ?, ?, ?)", (user_id, user_nickname, user_text, predict))
+    cursor_db.execute(f"""
+    INSERT INTO {database_name} (user_id, user_nickname, user_text, predict)
+    VALUES (?, ?, ?, ?)
+    """, (user_id, user_nickname, user_text, predict))
+
     connect_db.commit()
