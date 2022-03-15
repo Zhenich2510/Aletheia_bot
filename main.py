61b076b374c575c61a3a944ede1e2e206fc19f65
yevheniy <yevhen.01@gmail.com>
2022-03-15 16:26:11 +0200

    Connect to database.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from aiogram.utils import executor
 
 from start_bot import dp
-from handlers import other, user, admin
 from start_bot import on_startup
-
+from handlers import other, user, admin
+from database import start_db
 
 def main():
-    print('new row')
+    start_db()
     user.register_handlers_user(dp)
     admin.register_handlers_admin(dp)
     # other.register_handlers_other(dp)
 
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
     pass
+
 
 if __name__ == '__main__':
     main()
