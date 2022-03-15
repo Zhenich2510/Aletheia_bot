2c19f248eeaf292bf221853b751b152e0aec571b
yevheniy <yevhen.01@gmail.com>
2022-03-15 16:28:14 +0200

    Create 2 functions "start_db" and "add_info_to_db".

---
+++ database/__init__.py
@@ -0,0 +1,1 @@
+from database.init_database import start_db, add_info_to_db
---
+++ database/init_database.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+from conf import link_to_bd
+
+
+def start_db():
+    global connect_db, cursor_db
+    connect_db = sqlite3.connect(link_to_bd)
+    cursor_db = sqlite3.cursor()
+
+    if connect_db:
+        print("Database is work.")
+
+    connect_db.execute("""CREATE TABLE IF NOT EXIST db(
+    id INTEGER PRIMARY KEY, 
+    user_id INTEGER NOT NULL,
+    user_nickname CHAR(20) NOT NULL,
+    user_text TEXT NOT NULL,
+    predict BOOL NOT NULL
+    )
+    """)
+
+    connect_db.commit()
+
+
+def add_info_to_db(user_id: int, user_nickname: str, user_text: str, predict: bool):
+    cursor_db.execute("INSERT INTO db VALUES (?, ?, ?, ?)", (user_id, user_nickname, user_text, predict))
+    connect_db.commit()
